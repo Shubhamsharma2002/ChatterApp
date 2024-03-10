b460317e9dfab82a98ec67ba8b8cc4af6621bc00
Shubham Sharma <shubhamtech004@gmail.com>
2024-03-10 17:08:53 +0530

    Adding db in app to store msg

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import  express  from "express";
 import { Server } from "socket.io";
 import cors from "cors";
 import http from "http"
+import { connect}  from "./mongoose.js";
+import { chatModel } from "./chatSchema.js";
 
 const app = express();
 // 1. cretae server using http
@@ -21,9 +23,30 @@ const io = new Server(server, {
 
 io.on('connection', (Socket) =>{
     console.log('Connection is established');
+    Socket.on("join", (data) =>{
+        Socket.username = data;
+
+        chatModel.find().sort({timestamp:1}).limit(30).then(message =>{
+            Socket.emit('load_message', message);
+        }).catch(err => {
+            console.log(err);
+        })
+    })
+    
     Socket.on('new_message', (message) =>{
         // broadcast this message to al the client
-        Socket.broadcast.emit('broadcast_message', message);
+        let usermessage = {
+            username :Socket.username,
+            message:message
+        }
+
+        const newChat = new chatModel({
+            username:Socket.username,
+            message:message,
+            timestamp:new Date()
+        });
+        newChat.save();
+        Socket.broadcast.emit('broadcast_message', usermessage);
     })
     Socket.on('disconnect' , () =>{
         console.log("Connection is disconnected");
@@ -32,4 +55,5 @@ io.on('connection', (Socket) =>{
 
 server.listen(3000, () =>{
     console.log("server is listning on port no 3000");
+    connect();
 })
